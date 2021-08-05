cb67e51e343829eb7b86984925b2939773574b54
J Andrew Scott <jascott@programmer.net>
2021-08-05 11:27:13 -0400

    Remove dead code

--- src/app/search/search-form.component.ts
+++ src/app/search/search-form.component.ts
@@ -24,70 +24,7 @@ export class SearchFormComponent {
 	resultsEvent = new EventEmitter<object[]>();
 	results = [] as object[];
 
-	constructor(private api: ApiService) {
-		// combineLatest([
-		// 	svc.contacts,
-		// 	svc.subscribers
-		// ]).subscribe(([cons, subs]) => {
-		// 	const customers: {[id:string]:any} = {};
-		// 	const subscriptions: {[id:string]:any} = {};
-		//
-		// 	cons.forEach((con: any) => {
-		// 		customers[con.Id] = {
-		// 			Id: con.Id,
-		// 			Name: con.Name,
-		// 			Email: { SalesCloud: con.Email },
-		// 			Status: { SalesCloud: con.Status },
-		// 			Subscriptions: {}
-		// 		}
-		//
-		// 		con.Subscriptions.forEach((sub: any) => {
-		// 			const key = `${sub.ContactId}:${sub.GlobalProductCode}`;
-		// 			customers[con.Id].Subscriptions[key] = subscriptions[key] = {
-		// 				ContactId: sub.ContactId,
-		// 				ProductCode: sub.GlobalProductCode,
-		// 				Status: { SalesCloud: sub.Status },
-		// 				InactiveReason: { SalesCloud: sub.InactiveReason }
-		// 			};
-		// 		});
-		// 	});
-		//
-		// 	subs.forEach((sub: any) => {
-		// 		const cust = customers[sub.SubscriberKey] || {
-		// 			Id: sub.SubscriberKey,
-		// 			Email: { SalesCloud: undefined },
-		// 			Status: { SalesCloud: undefined },
-		// 			Subscriptions: {}
-		// 		};
-		//
-		// 		cust.Email.MarketingCloud = sub.EmailAddress;
-		// 		cust.Status.MarketingCloud = sub.Status;
-		//
-		// 		customers[sub.SubscriberKey] = cust;
-		//
-		// 		sub.Lists.forEach((listSub: any) => {
-		// 			if (listSub.ListClassification == 'PublicationList') {
-		// 				const subKey = sub.SubscriberKey as string;
-		// 				const key = `${subKey}:${listSub.ListCode}`;
-		//
-		// 				const custSub = subscriptions[key] || {
-		// 					ContactId: subKey,
-		// 					ProductCode: listSub.ListCode,
-		// 					Status: { SalesCloud: undefined },
-		// 					InactiveReason: { SalesCloud: undefined }
-		// 				};
-		//
-		// 				custSub.Status.MarketingCloud = listSub.Status;
-		// 				custSub.InactiveReason.MarketingCloud = listSub.InactiveReason;
-		//
-		// 				customers[subKey].Subscriptions[key] = subscriptions[key] = custSub;
-		// 			}
-		// 		});
-		// 	});
-		//
-		// 	console.log(customers);
-		// });
-	}
+	constructor(private api: ApiService) {}
 
 	search() {
 		this.inputCtrl.markAsTouched();
@@ -106,16 +43,6 @@ export class SearchFormComponent {
 				sub.Contact = cons.find((c: any) => {
 					return c.Id == sub.SubscriberKey;
 				}) as any;
-
-				// if (sub.Contact !== undefined) {
-				// 	sub.Lists.forEach((list: any) => {
-				// 		if (list.ListClassification == 'PublicationList') {
-				// 			list.Subscription = sub.Contact.Subscriptions.find((s: any) => {
-				// 				return s.GlobalProductCode === list.ListCode;
-				// 			}) as any;
-				// 		}
-				// 	});
-				// }
 			});
 
 			this.results = subs;
@@ -134,12 +61,4 @@ export class SearchFormComponent {
 	onKeyPress(event: KeyboardEvent) {
 		if (event.keyCode == 13) this.search();
 	}
-
-	// protected next(data: object[] = []) {
-	// 	// data.forEach((sub: any) => sub.Events.sort((e1: any, e2: any) => {
-	// 	// 	return Date.parse(e2.EventDate) - Date.parse(e1.EventDate);
-	// 	// }));
-	//
-	// 	this.resultsEvent.emit(this.results = data);
-	// }
 }
